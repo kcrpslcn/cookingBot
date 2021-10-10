8f53da08b56e6a77fa37585b69f6ad9ad1369b6c
niclas Prock <niclasprock@gmail.com>
2021-10-10 15:59:26 +0200

    GIT: ignore build and vscode folder

--- build/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var test_1 = require("./test");
-console.log(test_1.fun());
--- build/test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.fun = void 0;
-function fun() {
-    return 'FuN2';
-}
-exports.fun = fun;
